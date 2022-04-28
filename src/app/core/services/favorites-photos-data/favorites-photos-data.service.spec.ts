3223ffdec98301a1c150a39a2b7c18a6416d28b7
Mariia Valko Epam <mariiavalkoepam@MacBook-Pro-Maria.local>
2022-04-28 23:18:17 +0300

    test: add tests to FavoritesPhotosDataService

--- src/app/core/services/favorites-photos-data/favorites-photos-data.service.spec.ts
+++ src/app/core/services/favorites-photos-data/favorites-photos-data.service.spec.ts
@@ -1,16 +1,83 @@
 import { TestBed } from '@angular/core/testing';
-
+import { firstValueFrom } from 'rxjs';
+import { PhotoData } from 'src/app/shared/models/photo-data.m';
 import { FavoritesPhotosDataService } from './favorites-photos-data.service';
 
 describe('FavoritesPhotosDataService', () => {
   let service: FavoritesPhotosDataService;
+  const photoList: PhotoData[] = [
+    {
+      id: '1',
+      imgUrl: 'test-url',
+    },
+    {
+      id: '2',
+      imgUrl: 'test-url2',
+    },
+  ];
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(FavoritesPhotosDataService);
+    service.favoritesPhotos$.next([...photoList]);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return list of photos', async () => {
+    const result = await firstValueFrom(service.getFavoritesPhotos());
+    expect(result).toStrictEqual(photoList);
+  });
+
+  it('should return empty array', async () => {
+    service.favoritesPhotos$.next([]);
+    const result = await firstValueFrom(service.getFavoritesPhotos());
+    expect(result).toStrictEqual([]);
+  });
+
+  it('should return photoItem by id', async () => {
+    const id = '2';
+    const result = await firstValueFrom(service.getFavoritePhoto(id));
+
+    expect(result).toStrictEqual(photoList[1]);
+  });
+
+  it('should return null by wrong id', async () => {
+    const id = '5';
+    const result = await firstValueFrom(service.getFavoritePhoto(id));
+
+    expect(result).toBeNull();
+  });
+
+  it('should add new photoItem to favorites photos', () => {
+    const photoItem: PhotoData = {
+      id: '3',
+      imgUrl: 'test-url3',
+    };
+    service.addToFavoritesPhotos(photoItem);
+    const result = service.favoritesPhotos$.getValue();
+
+    expect(result).toStrictEqual([...photoList, photoItem]);
+  });
+
+  it('should not add existing photoItem to favorites photos', () => {
+    const photoItem: PhotoData = {
+      id: '2',
+      imgUrl: 'test-url2',
+    };
+    service.addToFavoritesPhotos(photoItem);
+    const result = service.favoritesPhotos$.getValue();
+
+    expect(result).toStrictEqual([...photoList]);
+  });
+
+  it('should delete photoItem from favorites photos by id', () => {
+    const id = '2';
+    service.deleteFromFavoritesPhotos(id);
+    const result = service.favoritesPhotos$.getValue();
+
+    expect(result).toStrictEqual([photoList[0]]);
   });
 });
